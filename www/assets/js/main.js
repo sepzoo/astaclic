a359284412739db2e4a9bb63c730de450cd2f994
sepzoo <37437353+sepzoo@users.noreply.github.com>
2018-07-16 17:10:36 +0200

    Initial commit

---
+++ www/assets/js/main.js
@@ -0,0 +1,200 @@
+$.noConflict();
+
+
+
+jQuery(document).ready(function ($) {
+
+	"use strict";
+
+	[].slice.call(document.querySelectorAll('select.cs-select')).forEach(function (el) {
+		new SelectFx(el);
+	});
+
+	jQuery('.selectpicker').selectpicker;
+
+
+	$('#menuToggle').on('click', function (event) {
+		$('body').toggleClass('open');
+	});
+
+	$('.search-trigger').on('click', function (event) {
+		event.preventDefault();
+		event.stopPropagation();
+		$('.search-trigger').parent('.header-left').addClass('open');
+	});
+
+	$('.search-close').on('click', function (event) {
+		event.preventDefault();
+		event.stopPropagation();
+		$('.search-trigger').parent('.header-left').removeClass('open');
+	});
+
+	$('#button-table-aste').on('click', function () {
+		$('#dashboard').hide();
+		$('#aste-table').show();
+		AppName.addAsteTable(AppName.aste);
+	})
+
+	var AppName = {
+		aste: [],
+		socket: null,
+		init: function () {
+			/**
+			 * Click per attivare la 
+			 *      FUNZIONE DI LOGOUT
+			 */
+			$("#logout_buttons button").on("click", function () {
+				AppName.logout_function();
+				// do something
+			});
+		},
+
+		getSession: function (callback_function) {
+			$.get("/session", callback_function);
+		},
+
+		/**
+		 * FUNZIONE per 
+		 *      COLLEGARSI al SERVER
+		 */
+		connection_server: function (token, callback) {
+
+			console.log("Connessione in Corso...");
+			socket = io();
+			socket.on("connect", function () {
+				socket
+					.emit("authenticate", { token: token })
+					.on("authenticated", callback)
+					.on("unauthorized", function (msg) {
+						console.error(msg);
+					})
+			});
+		},
+
+		getValueFromLogin: function (username, password) {
+
+			username = $('#formLoginUsername').val();
+			password = $('#formLoginPassword').val();
+			AppName.username = username;
+
+			$.post("/login", { nickname: username, password: password }, function (data) {
+				console.log("/login", data)
+				if (data.result) {
+					//se il login vaa buon fine ed ho il token, lo uso per aprire la connessione
+					AppName.connection_server(data.token, function () {
+						// fai qualcosa quando la connessione è stata stabilita.
+					});
+				}
+				else {
+					console.log("Username o Password Sbagliata!");
+					if (data.notRegistered) {
+
+					}
+					// Visualizzi un messaggio di errore per i dati sbagliati. 
+				}
+
+				if (data === 'done') {
+					console.log("Richiesta effettuata con successo. Data = " + data)
+				}
+			});
+		},
+
+		// Funzione che server per verificare se la sessione esiste già
+		checkSessionLogin: function (data) {
+			if (data.token) {
+				// la sessione esiste perchè il token è stato creato.
+				console.log("/session", data);
+				AppName.username = data.nickname;
+				AppName.connection_server(data.token, function () {
+					// fai qualcosa dopo aver stabilito la connessione con il server.
+				});
+			}
+			else {
+				// fai qualcosa se il token non è stato creato, quindi la sessione non esiste.
+			}
+		},
+
+		/**
+		 * FUNZIONE per 
+		 *      LOGGARSI sul SITO.
+		 */
+		logging: function () {
+			var username;
+			var password;
+
+			$("#accedi").on("click", function () {
+				// Tramite questa funzione si fa il Login
+				AppName.getValueFromLogin(username, password);
+			});
+
+			AppName.getSession(function (data) {
+				AppName.checkSessionLogin(data);
+			});
+
+		},
+
+		/**
+		 * FUNZIONE per
+		 *      LOGOUT
+		 */
+		logout_function: function () {
+			socket.emit("logout_event", socket.id);
+			console.log("Client -> socket:", socket.id);
+			$.post("/logout", function (data) {
+				console.log("Client -> Risposta", data);
+				console.log("Client -> Sessione distrutta(?)");
+			});
+		},
+
+		sortByDate: function () {
+			$('#dashboard-table').tablesorter({
+				dataFormat: "uk",
+				sortList: [[1, 0]]
+			})
+		},
+
+		addAsteDashboard: function (data) {
+			AppName.aste = data;
+			data.forEach(function (snap) {
+				var element =
+					$('<tr>\
+					<th scope="row">'+ snap.title + '</th>\
+					<td>'+ snap.fine + '</td>\
+					<td>'+ snap.stato + '</td>\
+				</tr>');
+
+				$('#table-dashboard-body').append(element);
+			});
+			AppName.sortByDate();
+		},
+
+		addAsteTable: function () {
+			AppName.aste.forEach(function (snap) {
+				var element =
+					'<tr>\
+					<th scope="row">'+ snap.title + '</th>\
+					<td>'+ snap.fine + '</td>\
+					<td>'+ snap.vincitore + '</td>\
+					<td>'+ snap.rilancio_minimo + '</td>\
+					<td>'+ snap.valore_attuale + '</td>\
+					<td>'+ snap.stato + '</td>\
+					<td>\
+                     <button class="btn btn-outline-success">Vai</button>\
+                    </td>\
+				</tr>';
+
+				$('#table-aste-body').append(element);
+			});
+
+			$.fn.dataTable.moment('dd, MM Do, YYYY');
+			$('#bootstrap-data-table').dataTable();
+		},
+
+		get_aste_dashboard: function () {
+			$.get("/aste-dashboard", AppName.addAsteDashboard);
+		}
+	};
+	AppName.init();
+	AppName.logging();
+	AppName.get_aste_dashboard();
+});
